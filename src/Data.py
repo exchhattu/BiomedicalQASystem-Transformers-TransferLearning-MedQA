451f01a1294b0a749ec0035570841fbb8752aa13
exchhattu <rojan@live.com>
2020-01-15 18:27:23 -0500

    completed xml parsing.

--- src/Data.py
+++ src/Data.py
@@ -8,14 +8,22 @@ import xml.etree.ElementTree as etree
 class MedicalData: 
 
     class QuestionPair:
-        def __init__(self, id, question, answer):
-            m_questions = {} 
-            m_answers   = {} 
+        def __init__(self, id, question, answer, qtype):
+            self._id = id 
+            self._questions =  question 
+            self._answers   =  answer 
+            self._qtype = qtype 
 
 
     def __init__(self):
+        self._qa_pair = [] # list of question answer pair 
 
-    def parse_xml_input(self, s_path_to_file):
+    def dissect(self):
+        print("# of qa pairs: %d" %len(self._qa_pair))
+
+
+
+    def parse_xml_input(self, s_path_to_file, is_debug=False):
         """
         parse XML file, create a dictionary to hold question 
         answer pairing
@@ -29,11 +37,39 @@ class MedicalData:
         o_xml_tree = etree.parse(s_path_to_file) 
         # get root element 
         o_root = o_xml_tree.getroot() 
+        print(o_root.tag)
         
         # create empty list for news items 
         newsitems = [] 
                                    
         # iterate news items 
-        for item in o_root.findall('QAPairs'):
-            print(item)
+        for q_pairs in o_root.findall('QAPairs'):
+            for q_pair in q_pairs: # QApair  
+                for q_data in  q_pair: 
+                    s_question, s_answer, s_qtype = "", "", ""
+                    if q_data.tag == "Question":
+                        s_question = q_data.text 
+                    elif q_data.tag == "Answer":
+                        s_answer= q_data.text 
+
+                    # id and question type are attributes. just keep question type.
+                    for attrib in q_data.attrib.keys():
+                        if attrib == "qtype":
+                            s_qtype = q_data.attrib[attrib]
+                    o_qa = self.QuestionPair("", s_question, s_answer, s_qtype)
+                    self._qa_pair.append(o_qa)
+
+        if is_debug:
+            self.dissect()
+                    
+    def tokenize(self, is_word=True): 
+        """
+        Sentences is tokenized based on the input given
+        There will be two ways to tokenize - word and 
+        sentence.
+
+        param:
+            is_word - determines how tokenization is applied 
+        """
+
 
