703881b03c2ad356b76ce1823a78005a72799ca9
exchhattu <rojan@live.com>
2020-01-22 18:12:51 -0500

    prototype for test using pretrained data is finished.

--- src/MedQA.py
+++ src/MedQA.py
@@ -7,25 +7,29 @@ import argparse
 import logging
 import os
 
-from EDA import EDA 
+from InputData import InputData 
+from QADataModel import QaDataModel
 
 
 if __name__=="__main__":
-
     parser = argparse.ArgumentParser()
     ## Required parameters
     # parser.add_argument("--mode", default="transfer_model", type=str, required=True,
     #                     help="select one from (transfer_model, pretrained, fronzen)")
-    # parser.add_argument("--pretrained", default="XLnet", type=str, required=True,
+    # parser.add_argument("--use_pretrained", default="XLnet", type=str, required=True,
     #                     help="XLNet models or others")
-    # parser.add_argument("--test", default="None", type=str, required=True,
-    #                     help="path to test data")
-
-    # optional data
-    parser.add_argument("--eda", default=None, type=str, required=False,
-            help="analyze a training, validation, and test data, example train:filename, test:filename, validate:filename")
+    parser.add_argument("--data", default="None", type=str, 
+                            required=True,
+                            help="path to data directory")
 
     args = parser.parse_args()
-    if args.eda:
-        eda = EDA(args.eda)
+    data = InputData(args.data)
+    # I will be accessible to get EDA
+    data.merge_and_split_data(ratio="8:1:1")
+
+
+    qa_data_model = QaDataModel()
+    qa_data_model.predict_using_predefined_models(data._test_examples)
+
+    
 
