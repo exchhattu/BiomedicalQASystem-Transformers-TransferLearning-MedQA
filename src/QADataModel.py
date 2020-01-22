10ebf96ab03d3df6dc5be110a9344bdbb1555f2a
exchhattu <rojan@live.com>
2020-01-22 18:14:55 -0500

    wrote multiple functions to wrap a bridge between py-torch to a dataset

--- src/QADataModel.py
+++ src/QADataModel.py
@@ -2,11 +2,17 @@
 """
 https://github.com/huggingface/transformers/blob/master/examples/run_squad.py
 """
+import os
+import json
+import collections
+
+
 import torch
 from pytorch_transformers import XLNetConfig, XLNetModel, XLNetTokenizer, XLNetForQuestionAnswering
 # from pytorch_transformers import XLNetModel, XLNetTokenizer
 from pytorch_transformers.optimization import WarmupConstantSchedule
 from pytorch_transformers import AdamW
+from utils_squad import convert_examples_to_features
 
 from torch.utils.data import (DataLoader, RandomSampler, SequentialSampler, TensorDataset)
 
@@ -23,18 +29,58 @@ class QaDataModel:
     For training and testing a given data
     """
 
-    def __init__(self, train_dataset):
+    def __init__(self):
+        # self._qa_pairs = qa_pairs # list but not good practice
         self.init_params('xlnet', f_lr=2e-5, f_eps = 1e-8)
-        self.make_qa_model(train_dataset, epochs = 4 )
+        # self.make_qa_model(train_dataset, valid_dataset, epochs = 4)
+
+        self._max_seq_length      = 384
+        self._doc_stride          = 128
+        self._max_query_length    = 64
+
+    def _create_dataset(self, examples, evaluate=False, output_examples=False):
+        features = convert_examples_to_features(examples=examples,
+                                                tokenizer=self._tokenizer,
+                                                max_seq_length=self._max_seq_length,
+                                                doc_stride=self._doc_stride,
+                                                max_query_length=self._max_query_length,
+                                                is_training=not evaluate)
+
+        # Convert to Tensors and build dataset
+        all_input_ids = torch.tensor([f.input_ids for f in features], dtype=torch.long)
+        all_input_mask = torch.tensor([f.input_mask for f in features], dtype=torch.long)
+        all_segment_ids = torch.tensor([f.segment_ids for f in features], dtype=torch.long)
+        all_cls_index = torch.tensor([f.cls_index for f in features], dtype=torch.long)
+        all_p_mask = torch.tensor([f.p_mask for f in features], dtype=torch.float)
+
+        if evaluate:
+            all_example_index = torch.arange(all_input_ids.size(0), dtype=torch.long)
+            dataset = TensorDataset(all_input_ids, all_input_mask, all_segment_ids,
+                                    all_example_index, all_cls_index, all_p_mask)
+        else:
+            all_start_positions = torch.tensor([f.start_position for f in features], dtype=torch.long)
+            all_end_positions = torch.tensor([f.end_position for f in features], dtype=torch.long)
+            dataset = TensorDataset(all_input_ids, all_input_mask, all_segment_ids,
+                                    all_start_positions, all_end_positions,
+                                    all_cls_index, all_p_mask)
+
+        if output_examples:
+            return dataset, examples, features
+        return dataset
+
+
+
+
+    # def class 
 
     def init_params(self, model_name, f_lr=2e-5, f_eps = 1e-8):
         self._device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-        MODEL_CLASSES = { "xlnet": (XLNetConfig, XLNetForQuestionAnswering, XLNetTokenizer) }
+        MODEL_CLASSES   = { "xlnet": (XLNetConfig, XLNetForQuestionAnswering, XLNetTokenizer) }
         # self._config, self._model_class, self._tokenizer = MODEL_CLASSES[model_name]
         self._tokenizer = XLNetTokenizer.from_pretrained('xlnet-base-cased', do_lower_case=True)
-        self._config = XLNetConfig.from_pretrained('xlnet-base-cased', do_lower_case=True)
-        self._model = XLNetForQuestionAnswering.from_pretrained('xlnet-base-cased', config=self._config)
+        self._config    = XLNetConfig.from_pretrained('xlnet-base-cased', do_lower_case=True)
+        self._model     = XLNetForQuestionAnswering.from_pretrained('xlnet-base-cased', config=self._config)
 
         param_optimizer = self._model.named_parameters()
 
@@ -49,23 +95,26 @@ class QaDataModel:
         self._optimizer = AdamW(optimizer_grouped_parameters, lr=f_lr, eps=f_eps)
 
 
-    def make_qa_model(self, train_dataset, epochs = 4):
-        train_batch_size = 8 # args.per_gpu_train_batch_size * max(1, args.n_gpu) # taken from ...
-        train_sampler = RandomSampler(train_dataset) # if args.local_rank == -1 else DistributedSampler(train_dataset)
-        train_dataloader = DataLoader(train_dataset, sampler=train_sampler, batch_size=train_batch_size)
+    def do_train(self, train_example, valid_example, epochs = 4):
+        train_dataset = self._create_dataset(train_examples, evaluate=False)
+        valid_dataset = self._create_dataset(valid_examples, evaluate=False)
+
+        train_batch_size    = 8 # args.per_gpu_train_batch_size * max(1, args.n_gpu) # taken from ...
+        train_sampler       = RandomSampler(train_dataset) # if args.local_rank == -1 else DistributedSampler(train_dataset)
+        train_dataloader    = DataLoader(train_dataset, sampler=train_sampler, batch_size=train_batch_size)
 
         max_step    = 1 # default value in run_squad.py
         t_total     = max_step 
         gradient_accumulation_steps = 1 # default value in run_squad.py
-        self._scheduler = get_linear_schedule_with_warmup(
-                            self._optimizer, num_warmup_steps=0 , # default value in run_squad.py 
-                            num_training_steps=t_total) # depending upon max_step; however default was chosen 
+        self._scheduler = get_linear_schedule_with_warmup(self._optimizer, num_warmup_steps=0 , # default value in run_squad.py 
+                                                            num_training_steps=t_total) # depending upon max_step; however default was chosen 
 
         global_step = 0
         tr_loss, logging_loss = 0.0, 0.0
 
         self._model.zero_grad()
         # set random seed.
+
         
         for _ in trange(epochs, desc="Iteration"):
             epoch_iterator = tqdm(train_dataloader, desc="Iteration")
@@ -74,6 +123,7 @@ class QaDataModel:
                 # what is args.device
                 batch = tuple(t.to(self._device) for t in batch)
 
+                # I should replace it
                 inputs = {'input_ids':       batch[0],
                           'attention_mask':  batch[1], 
                           'token_type_ids':  batch[2],  
@@ -84,12 +134,14 @@ class QaDataModel:
                 outputs = self._model(**inputs)
                 loss = outputs[0]  # model outputs are always tuple in pytorch-transformers (see doc)
                 loss.backward()
+                torch.nn.utils.clip_grad_norm_(self._model.parameters(), 1.0) # max_grad_norm = 1.0 by default
                 tr_loss += loss.item()
                 
                 # this function does not have effect unless gradient_accumulation_steps > 1
                 # however, it was set to 1 in original code.
                 # if (step + 1) % 1 == 0:
 
+                self._scheduler.step()  # Update learning rate schedule
                 self._optimizer.step()
                 self._model.zero_grad()
                 global_step += 1
@@ -104,21 +156,26 @@ class QaDataModel:
 
                 for batch in tqdm(eval_dataloader, desc="Evaluating"):
                     self._model.eval()
-                    batch = tuple(t.to(args.device) for t in batch)
+                    batch = tuple(t.to(self._device) for t in batch)
+                    print("Coding: length ", len(batch))
 
                     with torch.no_grad():
                         inputs = { "input_ids": batch[0],
                                    "attention_mask": batch[1],
                                    "token_type_ids": batch[2],
-                                   "cls_index": batch[4], 
-                                   "p_mask": batch[5]
+                                   "cls_index": batch[3], 
+                                   "p_mask": batch[4]
                                   }
-                        example_indices = batch[3]
+                        example_indices = batch[7]
                         outputs = self._model(**inputs)
 
                 for i, example_index in enumerate(example_indices):
-                #         eval_feature = features[example_index.item()]
-                #         unique_id = int(eval_feature.unique_id)
+                    # self._qa_pairs[] # donot confuse, this is member of this class; however I have to make accessible data without reinit.
+                    # eval_feature = features[example_index.item()]
+                    print("Coding: ", example_index)
+                    eval_feature = self._qa_pairs[example_index]
+                    print("Coding: ", eval_feature, outputs)
+                    # unique_id    = int(eval_feature.unique_id)
 
                 #         output = [to_list(output[i]) for output in outputs]
                 #         if len(output) >= 5:
@@ -214,4 +271,109 @@ class QaDataModel:
                 eval_accuracy += tmp_eval_accuracy
                 nb_eval_steps += 1
             print("Validation Accuracy: {}".format(eval_accuracy/nb_eval_steps))
-   
+
+    def write_json_file(self, examples, file_name = "predict.json"):
+        """
+        make json format of qa data and dump into a given file path
+
+        param:
+            examples: example file to be written 
+            file_name: file name  
+            is_debug: for testing purpose
+        """
+        # create a dictionary to dump as json 
+        d_final_data = collections.OrderedDict()
+        d_data = collections.OrderedDict()
+        t_data = []
+        for example in examples:
+            d_sub_data = collections.OrderedDict()
+            d_answer = collections.OrderedDict() 
+
+            d_answer = {'text': example.orig_answer_text, 
+                        'answer_start' : example.start_position }
+
+            d_sub_data = { "qas_id": example.qas_id,
+                           "question": example.question_text,
+                           "answers": [d_answer],
+                            }
+            d_data = {"qas": [d_sub_data] }
+            t_data.append(d_data)
+        d_paragraph = {"paragraphs": t_data} 
+        d_final_data = {"data": [d_paragraph]}
+        
+        # Save as a JSON file
+        full_path = os.path.join(os.getcwd(), file_name)
+        with open(full_path, 'w') as f:
+            json.dump(d_final_data, f)
+
+    def predict_using_predefined_models(self, test_example):
+        test_dataset = self._create_dataset(test_example, evaluate=True)
+
+        self.write_json_file(test_example, file_name = "predict.json")
+
+        # has to dump json file  
+        global_step = 1 
+        self.evaluate(test_dataset, self._model, self._tokenizer, prefix=global_step)
+
+
+    def evaluate(self, dataset, model, tokenizer, prefix=1):
+        # args.eval_batch_size = args.per_gpu_eval_batch_size * max(1, args.n_gpu)
+        eval_batch_size = 8 # by default # args.per_gpu_eval_batch_size * max(1, args.n_gpu)
+        # Note that DistributedSampler samples randomly
+        eval_sampler = SequentialSampler(dataset) # if args.local_rank == -1 else DistributedSampler(dataset)
+        eval_dataloader = DataLoader(dataset, sampler=eval_sampler, batch_size=eval_batch_size)
+        s_pred_file = os.path.join(os.getcwd(), "predict.json")
+
+        all_results = []
+        for batch in tqdm(eval_dataloader, desc="Evaluating"):
+            model.eval()
+            batch = tuple(t.to(self._device) for t in batch)
+            with torch.no_grad():
+                inputs = {'input_ids':      batch[0],
+                          'attention_mask': batch[1],
+                          'token_type_ids': batch[2],  
+                          'cls_index':      batch[4],
+                          'p_mask':         batch[5]
+                         }
+                example_indices = batch[3]
+                outputs = model(**inputs)
+
+        for i, example_index in enumerate(example_indices):
+            eval_feature = features[example_index.item()]
+            unique_id = int(eval_feature.unique_id)
+            # XLNet uses a more complex post-processing procedure
+            result = RawResultExtended(unique_id = unique_id,
+                                        start_top_log_probs  = to_list(outputs[0][i]),
+                                        start_top_index      = to_list(outputs[1][i]),
+                                        end_top_log_probs    = to_list(outputs[2][i]),
+                                        end_top_index        = to_list(outputs[3][i]),
+                                        cls_logits           = to_list(outputs[4][i]))
+            all_results.append(result)
+
+        # Compute predictions
+        output_dir = os.getcwd()
+        output_prediction_file = os.path.join(output_dir, "predictions_{}.json".format(prefix))
+        output_nbest_file = os.path.join(args.output_dir, "nbest_predictions_{}.json".format(prefix))
+        # if args.version_2_with_negative:
+        #     output_null_log_odds_file = os.path.join(args.output_dir, "null_odds_{}.json".format(prefix))
+        # else:
+        #     output_null_log_odds_file = None
+
+        # XLNet uses a more complex post-processing procedure
+        n_best_size = 20
+        max_answer_length = 30
+        output_null_log_odds_file = None
+        version_2_with_negative = True
+        verbose_logging = True
+        write_predictions_extended(examples, features, all_results, n_best_size,
+                                    max_answer_length, output_prediction_file,
+                                    output_nbest_file, output_null_log_odds_file, args.predict_file,
+                                    model.config.start_n_top, model.config.end_n_top,
+                                    version_2_with_negative, tokenizer, verbose_logging)
+
+        # Evaluate with the official SQuAD script
+        evaluate_options = EVAL_OPTS(data_file=s_pred_file,
+                                    pred_file=output_prediction_file,
+                                    na_prob_file=output_null_log_odds_file)
+        results = evaluate_on_squad(evaluate_options)
+        return results
