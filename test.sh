aca4533cab4b6622a9cb5459925a29dbdffe5d93
exchhattu <rojan@live.com>
2020-01-23 15:13:40 -0500

    Corrected path to MedQA.py

--- test.sh
+++ test.sh
@@ -6,4 +6,7 @@ python3 ./src/MedQA.py --eda train:./unittest/test3_json/BioASQ_factoid-6b.json
 
 python3 ./src/MedQA.py --data ./data/dataset/curated_BioASQ_7b/
 
-python3 ./src/MedQA.py --data ./unittest/test4_json/BioASQ-train-list-7b-snippet-annotated.json 
+python3 ./src/MedQA.py --data ./unittest/test4_json/
+
+python3 ./src/MedQA.py --args.end_to_end_tl --data ./unittest/test4_json/
+    
