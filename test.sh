109283847668ad75e17a07a058f4675fb3f0fdfa
exchhattu <rojan@live.com>
2020-01-27 07:21:02 -0500

    updated test.sh

--- test.sh
+++ test.sh
@@ -4,9 +4,11 @@ python3 ./src/test.py
 
 python3 ./src/MedQA.py --eda train:./unittest/test3_json/BioASQ_factoid-6b.json
 
-python3 ./src/MedQA.py --data ./data/dataset/curated_BioASQ_7b/
 
 python3 ./src/MedQA.py --data ./unittest/test4_json/
 
-python3 ./src/MedQA.py --args.end_to_end_tl --data ./unittest/test4_json/
+# use pretrained model
+python3 ./src/MedQA.py --use_pretrained_model --data ./data/dataset/curated_BioASQ_7b/
+
+python3 ./src/MedQA.py --end_to_end --data ./unittest/test4_json/
     
