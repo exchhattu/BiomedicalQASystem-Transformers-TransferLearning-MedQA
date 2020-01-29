f64658d5b3a90a45fd0008ee9e1a28501de5486f
exchhattu <rojan@live.com>
2020-01-28 21:58:22 -0500

    added random sampling procedure for training.

--- src/MedQA.py
+++ src/MedQA.py
@@ -6,9 +6,30 @@ Tue Jan 21 16:04:06 2020
 import argparse
 import logging
 import sys, os
+import subprocess
 
 from InputData import InputData 
 from QADataModel import QaDataModel
+
+#                                       --train_file $SQUAD_DIR/003/train.json \
+#                                             --predict_file $SQUAD_DIR/003/valid.json \
+#                                                   --do_train \
+#                                                         --do_eval \
+
+def train_valid_cmd(path_to_train, path_to_test):
+    program_path = os.path.join(os.getcwd(), "/src/run_squad.py") 
+    if not os.path.exists(program_path):
+        print("FATAL: program %s not found" %program_path)
+        sys.exit()
+
+    default_args = ( "python3",  program_path, \
+              "--overwrite_cache",  "--overwrite_output_dir", "--model_type", "xlnet", 
+              "--model_name_or_path", "xlnet-base-cased",  
+              "--do_lower_case",  "--learning_rate", "3e-5",  
+              "--num_train_epochs", "5", "--max_seq_length", "192", 
+              "--doc_stride", "128", "--train_file", path_to_train,
+              "--predict_file", path_to_test, "--do_train", "--do_eval")
+    return default_args
 
 
 if __name__=="__main__":
@@ -25,16 +46,26 @@ if __name__=="__main__":
                         help='Modify the hyperparameter and parameters')
 
     args = parser.parse_args()
+    print(os.getcwd())
+    
+
+
     data = InputData(args.data)
     # this function will generate - data._test_examples, data._train_examples, data._valid_examples
     data.merge_and_split_data(ratio="9.00:1.00:0.00", write_file=True)
     # sys.exit()
+    path_to_train = ""
+    path_to_valid = ""
+    st_cmd = train_valid_cmd(path_to_train, path_to_test):
 
     qa_data_model = QaDataModel()
     if args.use_pretrained_model: 
         qa_data_model.predict_using_predefined_models(data._test_examples)
     elif args.end_to_end:
-        print("Coding: I entered ")
-        qa_data_model.do_end_to_end(data._train_examples, data._valid_examples)
+        popen = subprocess.Popen(st_cmd, stdout=subprocess.PIPE)
+        popen.wait()
+        # output = popen.stdout.read()
+        # print("Coding: I entered ")
+        # qa_data_model.do_end_to_end(data._train_examples, data._valid_examples)
 
 
