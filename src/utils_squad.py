b9a3e70de1d1a7e9f6cd8c679b9277d7ea8b0774
exchhattu <rojan@live.com>
2020-02-02 11:28:29 -0500

    prototype of new class

--- src/utils_squad.py
+++ src/utils_squad.py
@@ -71,6 +71,48 @@ class SquadExample(object):
             s += ", is_impossible: %r" % (self.is_impossible)
         return s
 
+class SquadExampleExtended(object):
+    """
+    A single training/test example for the Squad dataset.
+    For examples without an answer, the start and end position are -1.
+    """
+
+    def __init__(self,
+                 qas_id,
+                 question_text,
+                 doc_tokens,
+                 orig_answer_text=None,
+                 start_position=None,
+                 end_position=None,
+                 paragraph = None,
+                 orig_start_position = None,
+                 is_impossible=None):
+        self.qas_id = qas_id
+        self.question_text = question_text
+        self.doc_tokens = doc_tokens
+        self.orig_answer_text = orig_answer_text
+        self.start_position = start_position
+        self.end_position = end_position
+        self.context = paragraph
+        self.orig_start_position = orig_start_position
+        self.is_impossible = is_impossible
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __repr__(self):
+        s = ""
+        s += "qas_id: %s" % (self.qas_id)
+        s += ", question_text: %s" % (
+            self.question_text)
+        s += ", doc_tokens: [%s]" % (" ".join(self.doc_tokens))
+        if self.start_position:
+            s += ", start_position: %d" % (self.start_position)
+        if self.end_position:
+            s += ", end_position: %d" % (self.end_position)
+        if self.is_impossible:
+            s += ", is_impossible: %r" % (self.is_impossible)
+        return s
 
 class InputFeatures(object):
     """A single set of features of data."""
@@ -107,8 +149,7 @@ class InputFeatures(object):
         self.end_position = end_position
         self.is_impossible = is_impossible
 
-
-def read_squad_examples(input_file, is_training, version_2_with_negative):
+def read_squad_examples_extended(input_file, is_training, version_2_with_negative):
     """Read a SQuAD json file into a list of SquadExample."""
     with open(input_file, "r", encoding='utf-8') as reader:
         input_data = json.load(reader)["data"]
@@ -120,9 +161,11 @@ def read_squad_examples(input_file, is_training, version_2_with_negative):
     
     invalid_count = 0
     recover_count = 0
+    total_count = 0
     examples = []
     for entry in input_data:
         for paragraph in entry["paragraphs"]:
+            total_count += 1
             paragraph_text = paragraph["context"]
             doc_tokens = []
             char_to_word_offset = []
@@ -175,9 +218,9 @@ def read_squad_examples(input_file, is_training, version_2_with_negative):
                             s_idx, e_idx = parse_further(cleaned_answer_text, doc_tokens)
                             if s_idx == -1 and e_idx == -1: 
                                 invalid_count += 1
-                                # print("Coding: still invalid ", qas_id, actual_text, cleaned_answer_text, orig_answer_text, s_idx, e_idx )
+                                print("WARN: still invalid ", qas_id) 
                                 continue
-                            # print("Coding: reovered ", qas_id, actual_text, cleaned_answer_text, orig_answer_text, s_idx, e_idx, start_position, end_position, doc_tokens)
+                            print("WARN: reovered ", qas_id) 
                             start_position = s_idx
                             end_position = e_idx
                             recover_count += 1
@@ -187,7 +230,105 @@ def read_squad_examples(input_file, is_training, version_2_with_negative):
                         end_position = -1
                         orig_answer_text = ""
 
-                # print("Coding: exmaple work ", qas_id, orig_answer_text, start_position, end_position, doc_tokens)
+                print("INFO: example work ", qas_id) 
+                example = SquadExampleExtended(
+                    qas_id=qas_id,
+                    question_text=question_text,
+                    doc_tokens=doc_tokens,
+                    orig_answer_text=orig_answer_text,
+                    start_position=start_position,
+                    end_position=end_position,
+                    paragraph=paragraph_text,
+                    orig_start_position = answer_offset,
+                    is_impossible=is_impossible)
+                examples.append(example)
+    print("INFO: invalid count ", invalid_count)
+    print("INFO: recovered count", recover_count) 
+    print("INFO: totalcount ", total_count)
+    return examples
+
+
+def read_squad_examples(input_file, is_training, version_2_with_negative):
+    """Read a SQuAD json file into a list of SquadExample."""
+    with open(input_file, "r", encoding='utf-8') as reader:
+        input_data = json.load(reader)["data"]
+
+    def is_whitespace(c):
+        if c == " " or c == "\t" or c == "\r" or c == "\n" or ord(c) == 0x202F:
+            return True
+        return False
+    
+    invalid_count = 0
+    recover_count = 0
+    total_count = 0
+    examples = []
+    for entry in input_data:
+        for paragraph in entry["paragraphs"]:
+            total_count += 1
+            paragraph_text = paragraph["context"]
+            doc_tokens = []
+            char_to_word_offset = []
+            prev_is_whitespace = True
+            for c in paragraph_text:
+                if is_whitespace(c):
+                    prev_is_whitespace = True
+                else:
+                    if prev_is_whitespace:
+                        doc_tokens.append(c)
+                    else:
+                        doc_tokens[-1] += c
+                    prev_is_whitespace = False
+                char_to_word_offset.append(len(doc_tokens) - 1)
+
+            for qa in paragraph["qas"]:
+                qas_id = qa["id"]
+                question_text = qa["question"]
+                start_position = None
+                end_position = None
+                orig_answer_text = None
+                is_impossible = False
+                if is_training:
+                    if version_2_with_negative:
+                        is_impossible = qa["is_impossible"]
+                    if (len(qa["answers"]) != 1) and (not is_impossible):
+                        raise ValueError(
+                            "For training, each question should have exactly 1 answer.")
+                    if not is_impossible:
+                        answer = qa["answers"][0]
+                        orig_answer_text = answer["text"]
+                        answer_offset = answer["answer_start"]
+                        answer_length = len(orig_answer_text)
+                        start_position = char_to_word_offset[answer_offset]
+                        end_position = char_to_word_offset[answer_offset + answer_length - 1]
+                        # Only add answers where the text can be exactly recovered from the
+                        # document. If this CAN'T happen it's likely due to weird Unicode
+                        # stuff so we will just skip the example.
+                        #
+                        # Note that this means for training mode, every example is NOT
+                        # guaranteed to be preserved.
+                        actual_text = " ".join(doc_tokens[start_position:(end_position + 1)])
+                        cleaned_answer_text = " ".join(
+                            whitespace_tokenize(orig_answer_text))
+                        if actual_text.find(cleaned_answer_text) == -1:
+                            logger.warning("Could not find answer: '%s' '%s' vs. '%s'",
+                                           qas_id, actual_text, cleaned_answer_text)
+
+                            # majority of data is kick out by this constraints
+                            s_idx, e_idx = parse_further(cleaned_answer_text, doc_tokens)
+                            if s_idx == -1 and e_idx == -1: 
+                                invalid_count += 1
+                                print("WARN: still invalid ", qas_id) 
+                                continue
+                            print("WARN: reovered ", qas_id) 
+                            start_position = s_idx
+                            end_position = e_idx
+                            recover_count += 1
+                            # continue
+                    else:
+                        start_position = -1
+                        end_position = -1
+                        orig_answer_text = ""
+
                 example = SquadExample(
                     qas_id=qas_id,
                     question_text=question_text,
@@ -198,7 +339,8 @@ def read_squad_examples(input_file, is_training, version_2_with_negative):
                     is_impossible=is_impossible)
                 examples.append(example)
     print("INFO: invalid count ", invalid_count)
-    print("INFO: recovered ", recover_count) 
+    print("INFO: recovered count", recover_count) 
+    print("INFO: totalcount ", total_count)
     return examples
 
 def parse_further(cleaned_answer_text, doc_tokens):
