bb6f11ceabafc71b9534d1b6a2442d39a2f863cf
exchhattu <rojan@live.com>
2020-02-02 11:26:59 -0500

    created new class, which handles ambugities on context and start position of answer

--- src/PyTorch_transform_wrapper.py
+++ src/PyTorch_transform_wrapper.py
@@ -3,9 +3,7 @@ Rojan Shrestha, Insight Data Science
 Tue Jan 21 21:36:32 2020
 """
 
-from utils_squad import (read_squad_examples, convert_examples_to_features,
-                         RawResult, write_predictions,
-                         RawResultExtended, write_predictions_extended)
+from utils_squad import read_squad_examples_extended 
 
 
 class PyTorchTransformWrapper:
@@ -14,4 +12,4 @@ class PyTorchTransformWrapper:
         self._qa_data = []
 
     def read_squad_formatted_json_file(self, path_to_file): 
-        self._qa_data = read_squad_examples(input_file=path_to_file, is_training=True, version_2_with_negative=False)
+        self._qa_data = read_squad_examples_extended(input_file=path_to_file, is_training=True, version_2_with_negative=False)
